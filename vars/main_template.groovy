a2ced9a6d4c6215de5cbd3ea71611827fcd19ba4
ankur1825 <kashyap.ankur0114@gmail.com>
2025-03-17 19:57:26 -0400

    Update main_template.groovy

--- vars/main_template.groovy
+++ vars/main_template.groovy
@@ -1,70 +1,71 @@
-// This should be at the top of the main_template.groovy
-//@Library('jenkins-shared-library@main') _
+// Refactor main_template.groovy to be callable as a function if needed
+def call(Map params) {
 
-pipeline {
-    agent any
-    parameters {
-        string(name: 'APP_TYPE', defaultValue: '', description: 'Select Application Type (Java, Spring Boot, Python, Docker, NPM, .NET)')
-        string(name: 'REPO_URL', defaultValue: '', description: 'Git Repository URL')
-        string(name: 'BRANCH', defaultValue: '', description: 'Git Branch')
-        booleanParam(name: 'ENABLE_SONARQUBE', defaultValue: false, description: 'Enable SonarQube Analysis')
-        booleanParam(name: 'ENABLE_OPA', defaultValue: false, description: 'Enable OPA Security Scan')
-        string(name: 'IMAGE_NAME', defaultValue: 'my-app', description: 'Docker Image Name')
-    }
+    pipeline {
+        agent any
+        parameters {
+            string(name: 'APP_TYPE', defaultValue: '', description: 'Select Application Type (Java, Spring Boot, Python, Docker, NPM, .NET)')
+            string(name: 'REPO_URL', defaultValue: '', description: 'Git Repository URL')
+            string(name: 'BRANCH', defaultValue: '', description: 'Git Branch')
+            booleanParam(name: 'ENABLE_SONARQUBE', defaultValue: false, description: 'Enable SonarQube Analysis')
+            booleanParam(name: 'ENABLE_OPA', defaultValue: false, description: 'Enable OPA Security Scan')
+            string(name: 'IMAGE_NAME', defaultValue: 'my-app', description: 'Docker Image Name')
+        }
 
-    stages {
-        stage('Select Application') {
-            steps {
-                script {
-                    def appType = params.APP_TYPE
-                    def repoUrl = params.REPO_URL
-                    
-                    if (!repoUrl) {
-                        error "Repository URL is missing. Please provide a valid URL."
-                    }
+        stages {
+            stage('Select Application') {
+                steps {
+                    script {
+                        def appType = params.APP_TYPE
+                        def repoUrl = params.REPO_URL
+                        
+                        if (!repoUrl) {
+                            error "Repository URL is missing. Please provide a valid URL."
+                        }
 
-                    stage('Clone Repository') {
-                        git url: repoUrl, branch: params.BRANCH
-                    }
+                        stage('Clone Repository') {
+                            git url: repoUrl, branch: params.BRANCH
+                        }
 
-                    stage('Validate Repository') {
-                        script {
-                            def detectedType = ''
+                        stage('Validate Repository') {
+                            script {
+                                def detectedType = ''
 
-                            if (fileExists('pom.xml')) {
-                                detectedType = 'Java'
-                            } else if (fileExists('requirements.txt')) {
-                                detectedType = 'Python'
-                            } else if (fileExists('package.json')) {
-                                detectedType = 'NPM'
-                            } else {
-                                error "Unsupported or unknown repository type."
-                            }
+                                if (fileExists('pom.xml')) {
+                                    detectedType = 'Java'
+                                } else if (fileExists('requirements.txt')) {
+                                    detectedType = 'Python'
+                                } else if (fileExists('package.json')) {
+                                    detectedType = 'NPM'
+                                } else {
+                                    error "Unsupported or unknown repository type."
+                                }
 
-                            if (appType != detectedType) {
-                                error "Mismatch: Selected app type is ${appType}, but repository seems to be ${detectedType}."
+                                if (appType != detectedType) {
+                                    error "Mismatch: Selected app type is ${appType}, but repository seems to be ${detectedType}."
+                                }
                             }
                         }
-                    }
 
-                    // Load the specific child pipeline based on app type
-                    if (appType == 'Java') {
-                        load 'jenkins_template/javaPipeline.groovy'
-                    } else if (appType == 'Spring Boot') {
-                        load 'jenkins_template/springBootPipeline.groovy'
-                    } else if (appType == 'Python') {
-                        load 'jenkins_template/pythonPipeline.groovy'
-                    } else if (appType == 'Docker') {
-                        load 'jenkins_template/dockerPipeline.groovy'
-                    } else if (appType == 'NPM') {
-                        load 'jenkins_template/npmPipeline.groovy'
-                    } else if (appType == '.NET') {
-                        load 'jenkins_template/dotnetPipeline.groovy'
-                    } else {
-                        error 'Unsupported application type'
+                        // Load the specific child pipeline based on app type
+                        if (appType == 'Java') {
+                            load 'jenkins_template/javaPipeline.groovy'
+                        } else if (appType == 'Spring Boot') {
+                            load 'jenkins_template/springBootPipeline.groovy'
+                        } else if (appType == 'Python') {
+                            load 'jenkins_template/pythonPipeline.groovy'
+                        } else if (appType == 'Docker') {
+                            load 'jenkins_template/dockerPipeline.groovy'
+                        } else if (appType == 'NPM') {
+                            load 'jenkins_template/npmPipeline.groovy'
+                        } else if (appType == '.NET') {
+                            load 'jenkins_template/dotnetPipeline.groovy'
+                        } else {
+                            error 'Unsupported application type'
+                        }
                     }
                 }
             }
         }
     }
-}
+}    
