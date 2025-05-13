ebce11c0307d6e777a279e20ca31577ef3539fac
Ankur Kashyap <kashyap.ankur0114@gmail.com>
2025-05-13 19:19:29 -0400

    update main_template.groovy

--- vars/main_template.groovy
+++ vars/main_template.groovy
@@ -10,8 +10,13 @@ def call(Map params) {
         error "Repository URL is missing. Please provide a valid URL."
     }
 
-    stage('Print Incoming Parameters') {
-        steps {
+    node {
+
+        stage('Clone shared library'){
+            git credentialsId: credentialsId, url: 'https://github.com/ankur1825/jenkins-shared-library.git', branch: 'main'
+        }
+
+        stage('Print Incoming Parameters') {
             script {
                 echo "==== Incoming Parameters ===="
                 params.each { key, value ->
@@ -20,34 +25,32 @@ def call(Map params) {
                 echo "=============================="
             }
         }
-    }
-    
-    stage('Clone shared library'){
-        git credentialsId: credentialsId, url: 'https://github.com/ankur1825/jenkins-shared-library.git', branch: 'main'
-    }
 
-    // Stage to check dir and content
-    stage('check current dir') {
-        sh 'pwd'
-        sh 'ls -lrth '
-    }
-
-    // Load and run the appropriate app-specific pipeline
-    script {
-
-        // @Library('jenkins-shared-library@main') _
-        def pipelineScript = null
-
-        if (appType == 'java') {
-            pipelineScript = load 'jenkins_template/AppPipeline.groovy'
-        } else if (appType == 'python') {
-            pipelineScript = load 'jenkins_template/AppPipeline.groovy'
-        } else if (appType == 'npm') {
-            pipelineScript = load 'jenkins_template/AppPipeline.groovy'
-        } else {
-            error "Unsupported application type: ${appType}. Please choose 'java', 'python', or 'npm'."
+        // Stage to check dir and content
+        stage('check current dir') {
+            sh 'pwd'
+            sh 'ls -lrth '
         }
 
-        pipelineScript.run(params)
+        // Load and run the appropriate app-specific pipeline
+        stage('Load and Run Application Pipeline') {
+            script {
+
+                // @Library('jenkins-shared-library@main') _
+                def pipelineScript = null
+
+                if (appType == 'java') {
+                    pipelineScript = load 'jenkins_template/AppPipeline.groovy'
+                } else if (appType == 'python') {
+                    pipelineScript = load 'jenkins_template/AppPipeline.groovy'
+                } else if (appType == 'npm') {
+                    pipelineScript = load 'jenkins_template/AppPipeline.groovy'
+                } else {
+                    error "Unsupported application type: ${appType}. Please choose 'java', 'python', or 'npm'."
+                }
+
+                pipelineScript.run(params)
+            }
+        }
     }
-}
+}        
