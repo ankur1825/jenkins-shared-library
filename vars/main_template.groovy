0252b029f6b1724d1e9e7e83e144978cc2adda4e
Ankur Kashyap <kashyap.ankur0114@gmail.com>
2025-05-23 00:47:54 -0400

    updated files with paramterized job

--- vars/main_template.groovy
+++ vars/main_template.groovy
@@ -1,50 +1,46 @@
-def call(Map params) {
-    // Always use params directly instead of copying to new variables
-    if (!params.REPO_URL) {
+def call() {
+    def credentialsId = params.CREDENTIALS_ID
+    def appType = params.APP_TYPE
+    def repoUrl = params.REPO_URL
+    def branch = params.BRANCH
+    def enableSonarQube = params.ENABLE_SONARQUBE.toBoolean() ?: false
+    def enableOpa = params.ENABLE_OPA.toBoolean() ?: false
+    def enableTrivy = params.ENABLE_TRIVY.toBoolean() ?: false
+
+    if (!repoUrl?.trim()) {
         error "Repository URL is missing. Please provide a valid URL."
     }
 
     node {
-
-        stage('Clone Shared Library') {
-            git credentialsId: params.CREDENTIALS_ID, url: 'https://github.com/ankur1825/jenkins-shared-library.git', branch: 'main'
+        stage('Clone shared library') {
+            git credentialsId: credentialsId, url: 'https://github.com/ankur1825/jenkins-shared-library.git', branch: 'main'
         }
 
         stage('Print Incoming Parameters') {
             script {
                 echo "==== Incoming Parameters ===="
-                params.each { key, value ->
-                    echo "${key} = ${value}"
-                }
+                echo "APP_TYPE = ${appType}"
+                echo "REPO_URL = ${repoUrl}"
+                echo "BRANCH = ${branch}"
+                echo "ENABLE_SONARQUBE = ${enableSonarQube}"
+                echo "ENABLE_OPA = ${enableOpa}"
+                echo "ENABLE_TRIVY = ${enableTrivy}"
                 echo "=============================="
             }
         }
 
-        stage('Check Workspace') {
+        stage('Check Directory') {
             sh 'pwd'
             sh 'ls -lrth'
         }
 
         stage('Load and Run Application Pipeline') {
-            script {
-                def appType = params.APP_TYPE?.toLowerCase()
-                def pipelineScript = null
-
-                switch (appType) {
-                    case 'java':
-                    case 'python':
-                    case 'npm':
-                        pipelineScript = load 'jenkins_template/AppPipeline.groovy'
-                        break
-                    default:
-                        error "Unsupported application type: ${appType}. Please choose 'java', 'python', or 'npm'."
-                }
-
-                pipelineScript.run(params)
-            }
+            def pipelineScript = load 'jenkins_template/AppPipeline.groovy'
+            pipelineScript.run(params)  // still pass params here for compatibility
         }
     }
 }
+
 
 
 
