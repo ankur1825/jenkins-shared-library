4b435b8572fb78387208e4fd1abaf849473c169c
ankur1825 <kashyap.ankur0114@gmail.com>
2025-08-19 10:54:08 -0400

    Update AppPipeline.groovy

--- jenkins_template/AppPipeline.groovy
+++ jenkins_template/AppPipeline.groovy
@@ -78,7 +78,7 @@ def run(Map params) {
                         }
                     }
 
-                    timeout(time: 2, unit: 'MINUTES') {
+                    timeout(time: 10, unit: 'MINUTES') {
                         waitForQualityGate abortPipeline: false
                     }
 
