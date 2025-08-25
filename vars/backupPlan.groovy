b47e85988025a72b67d934865ccdcd8269fca06e
ankur1825 <kashyap.ankur0114@gmail.com>
2025-08-25 14:13:53 -0400

    Update backupPlan.groovy

--- vars/backupPlan.groovy
+++ vars/backupPlan.groovy
@@ -1,12 +1,19 @@
+// vars/backupPlan.groovy
 def apply(Map m = [:]) {
   // m.tags, m.kmsAlias, m.copyToRegion
   def tfvars = [
-    tags: (m.tags ?: [:]) + [ "Backup" : "true" ],
-    kms_key_alias: (m.kmsAlias ?: "alias/tenant-data"),
-    copy_to_region: m.copyToRegion
+    // ensure Backup tag is always present
+    tags           : (m.tags ?: [:]) + [ "Backup" : "true" ],
+    kms_key_alias  : (m.kmsAlias ?: "alias/tenant-data"),
+    copy_to_region : m.copyToRegion  // can be null
   ]
+
   def tfvarsJson = groovy.json.JsonOutput.toJson(tfvars)
-  terraform.plan('orchestration/iac/stacks/aws/backup-plan', js)
+  echo "backupPlan tfvars: ${tfvarsJson}"
+
+  // 👇 pass the right variable
+  terraform.plan('orchestration/iac/stacks/aws/backup-plan', tfvarsJson)
   terraform.apply('orchestration/iac/stacks/aws/backup-plan')
 }
+
 return this
